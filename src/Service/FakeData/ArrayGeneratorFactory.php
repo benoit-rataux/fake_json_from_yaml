be2f2f87eef0b5d207fba633245d7f834ff77f5f
Benoît Rataux <inscription.sh2ujp@gmail.com>
2025-04-16 22:49:16 +0200

    ajout de la locale au faker.yaml

--- src/Service/FakeData/ArrayGeneratorFactory.php
+++ src/Service/FakeData/ArrayGeneratorFactory.php
@@ -3,9 +3,13 @@
 namespace App\Service\FakeData;
 
 use Faker\Factory;
+use Symfony\Component\Yaml\Yaml;
+
 class ArrayGeneratorFactory {
+    private static string $CONF_FILE = __DIR__.'/../../../conf/faker.yaml';
 
     public static function create(): ArrayGenerator {
-        return new ArrayGenerator(Factory::create());
+        $conf = Yaml::parseFile(self::$CONF_FILE);
+        return new ArrayGenerator(Factory::create($conf['locale']));
     }
 }
