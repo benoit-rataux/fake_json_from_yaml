72fe5632c64f6f902710de88e099910ddd6b5fef
Benoît Rataux <inscription.sh2ujp@gmail.com>
2025-04-16 23:05:26 +0200

    #REFAC rename init.yaml -> json_template.yaml

--- src/main.php
+++ src/main.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\Yaml\Yaml;
 use App\Service\FakeData\ArrayGeneratorFactory;
 
-$CONF_INIT_FILE = __DIR__.'/../conf/init.yaml';
+$CONF_INIT_FILE = __DIR__.'/../conf/json_template.yaml';
 
 $conf = Yaml::parseFile($CONF_INIT_FILE);
 $fileName = $conf['template']['name'];
